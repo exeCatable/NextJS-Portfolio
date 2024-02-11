574f69ebcec301c2db387953a23a2bcb8e4246ce
Büsra Sahin <buesra.sahin97@web.de>
2024-02-11 14:55:23 +0100

    refactor header component

--- components/header.tsx
+++ components/header.tsx
@@ -1,3 +1,4 @@
+// for functionality of FramerMotion
 "use client";
 
 import React from "react";
@@ -6,6 +7,22 @@ import { links } from "@/lib/data";
 import Link from "next/link";
 
 export default function Header() {
+  const listElement = links.map((link) => (
+    <motion.li
+      className="flex h-3/4 items-center justify-center"
+      key={link.hash}
+      initial={{ y: -100, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+    >
+      <Link
+        className="flex w-full items-center justify-center px-3 py-3 transition hover:text-gray-950"
+        href={link.hash}
+      >
+        {link.name}
+      </Link>
+    </motion.li>
+  ));
+
   return (
     <header className="z-[999] relative">
       <motion.div
@@ -21,16 +38,7 @@ export default function Header() {
           className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial]
         sm:flex-nowrap sm:gap-5"
         >
-          {links.map((link) => (
-            <motion.li
-              className="h-3/4 flex items-center justify-center"
-              key={link.hash}
-              initial={{ y: -100, opacity: 0 }}
-              animate={{ y: 0, opacity:1 }}
-            >
-              <Link className="flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition" href={link.hash}>{link.name}</Link>
-            </motion.li>
-          ))}
+          {listElement}
         </ul>
       </nav>
     </header>
