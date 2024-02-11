ffdb5d3bd38bc8df52099f34254e5fc90fa0ef49
Büsra Sahin <buesra.sahin97@web.de>
2024-02-11 23:58:56 +0100

    add projects

---
+++ components/projects.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import React from "react";
+import SectionHeading from "./section-heading";
+import { projectsData } from "@/lib/data";
+import Project from "./project";
+
+export default function Projects() {
+  return (
+    <section className="mb-28 scroll-mt-28">
+      <SectionHeading>My projects</SectionHeading>
+      <div>
+        {projectsData.map((project, index) => (
+          <React.Fragment key={index}>
+            <Project {...project} />
+          </React.Fragment>
+        ))}
+      </div>
+    </section>
+  );
+}
